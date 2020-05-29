522ff1c56475f5658905dd219f1b49a525946b26
Steve Brett <steve_brett@pandoratek.org>
2020-05-29 20:00:52 +0100

    WIP

--- rgbled.c
+++ rgbled.c
@@ -16,10 +16,12 @@
 #define MAP_MASK (MAP_SIZE - 1)
 
 static int memfd = 0;
+void *mapped_dev_base;
 
+int openkmem () {	
+	void 	*mapped_base;
+	off_t 	dev_base = RGBLED_BASE_ADDRESS;
 
-
-int openkmem () {
 	memfd = open("/dev/mem", O_RDWR | O_SYNC);
     if (memfd == -1) {
 		printf("Can't open /dev/mem.\n");
@@ -27,15 +29,34 @@ int openkmem () {
 	}
 	printf("/dev/mem opened.\n");
 
+	// Map one page of memory into user space such that the device is in that page, but it may not
+	// be at the start of the page
+	mapped_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memfd, dev_base & ~MAP_MASK);
+    	if (mapped_base == (void *) -1) {
+		printf("Can't map the memory to user space.\n");
+		return FAILED;
+	}
+ 	printf("Memory mapped at address %p.\n", mapped_base);
+
+	// get the address of the device in user space which will be an offset from the base 
+	// that was mapped as memory is mapped at the start of a page 
+	mapped_dev_base = mapped_base + (dev_base & MAP_MASK);
+
+	return SUCCESS;
+}
+
+int closekmem() {
+	if (munmap(mapped_base, MAP_SIZE) == -1) {
+		printf("Can't unmap memory from user space.\n");
+		return FAILED;
+	}
+	close(memfd);
 	return SUCCESS;
 }
 
 
 int main() {
-
 	int 	rc;
-	void 	*mapped_base, *mapped_dev_base; 
-	off_t 	dev_base = RGBLED_BASE_ADDRESS; 
 
 	if (memfd == 0) {
 		rc = openkmem();
@@ -44,31 +65,10 @@ int main() {
 		}
 	}
 
-
-	// Map one page of memory into user space such that the device is in that page, but it may not
-	// be at the start of the page
-
-	mapped_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memfd, dev_base & ~MAP_MASK);
-    	if (mapped_base == (void *) -1) {
-		printf("Can't map the memory to user space.\n");
-		exit(0);
-	}
- 	printf("Memory mapped at address %p.\n", mapped_base); 
-
-	// get the address of the device in user space which will be an offset from the base 
-	// that was mapped as memory is mapped at the start of a page 
-
-	mapped_dev_base = mapped_base + (dev_base & MAP_MASK);
-
 	*((volatile unsigned long *) (mapped_dev_base  + RGBLED_RED)) = 5000;
-	*((volatile unsigned long *) (mapped_dev_base  + RGBLED_BLU)) = 5000;
+	*((volatile unsigned long *) (mapped_dev_base  + RGBLED_BLU)) = 9500;
 
+	closekmem();
 
-	if (munmap(mapped_base, MAP_SIZE) == -1) {
-		printf("Can't unmap memory from user space.\n");
-		exit(0);
-	}
-
-	close(memfd);
 	return 0;
 } 
